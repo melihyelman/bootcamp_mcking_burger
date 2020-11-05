7093b702dcbaac5168f71550bfb132998298281c
melihyelman <myelman17@gmail.com>
2020-11-05 20:55:33 +0300

    Create app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from "react";
+import IngredientsContext from "./context/IngredientsContext";
+import Container from "./components/Container/Container";
+import Header from "./components/Header/Header";
+import Content from "./components/Content/Content";
 
 const App = () => {
-  return <p>Baslangıç Noktası</p>;
+  const [ingredients, setIngredients] = useState({
+    cheese: 0,
+    salad: 0,
+    meat: 0,
+  });
+
+  return (
+    <IngredientsContext.Provider value={{ ingredients, setIngredients }}>
+      <Container>
+        <Header />
+        <Content />
+      </Container>
+    </IngredientsContext.Provider>
+  );
 };
 
 export default App;
